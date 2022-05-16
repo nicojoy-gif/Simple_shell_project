c6e3f1840892e084b4bee2d5149277c0afaa8bad
nicojoy-gif <67021106+nicojoy-gif@users.noreply.github.com>
2022-05-16 00:09:23 -0700

    Update fork.c

--- fork.c
+++ fork.c
@@ -5,21 +5,22 @@
 
 int main(int argc, char **argv)
 {
-pid_t pid;
-pid = fork();
-if(pid==0)
-{
-printf("It is the child process and pid is %d\n",getpid());
-exit(0);
-}
-else if(pid > 0)
-{
-printf("It is the parent process and pid is %d\n",getpid());
-}
-else
-{
-printf("Error while forking\n");
-exit(EXIT_FAILURE);
-}
-return 0;
-}
+    pid_t pid;
+    pid = fork();
+  
+    if(pid==0)
+    {
+        printf("It is the child process and pid is %d\n",getpid());
+        exit(0);
+    }
+    else if(pid > 0)
+    {
+        printf("It is the parent process and pid is %d\n",getpid());
+    }
+    else
+    {
+        printf("Error while forking\n");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+    }
