638ff62021d0b5d14fb8be7c3f10a1a73f73dc69
nicojoy-gif <67021106+nicojoy-gif@users.noreply.github.com>
2022-05-15 23:19:30 -0700

    Update getenv.c
    get environmental variable

--- getenv.c
+++ getenv.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <alloc.h>
-#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include main.h
 
 /**
- *
+ * _getenv - get environmental variable
+ * @name: character used
+ * Return: environmental value
+ */
+char *_getenv(const char *name)
+{
+  int cLoop = 0;
+
+	while (environ[cLoop] != NULL)
+	{
+    if (_strstr(environ[cLoop], name, 1) != NULL)
+		{
+			return (environ[cLoop]);
+		}
+		cLoop++;
+	}
+
+	return (NULL);
+}
